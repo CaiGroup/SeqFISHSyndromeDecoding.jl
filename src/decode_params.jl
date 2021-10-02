ca99f989eae624941ca96e6ce35ca0feb82222e5
Jonathan White <jawhite@caltech.edu>
2021-10-02 16:33:19 -0700

    Update decode_params.jl

--- src/decode_params.jl
+++ src/decode_params.jl
@@ -1,7 +1,7 @@
 export set_xy_search_radius, set_z_search_radius, set_n_allowed_drops, set_lat_var_cost_coeff,
 set_z_var_cost_coeff, set_lw_var_cost_coeff, set_s_var_cost_coeff,
 set_mip_sa_thresh, set_free_dot_cost, set_n_chains, set_l_chains, set_cooling_factor,
-set_cooling_timescale, set_erasure_penalty, set_converge_multiplier
+set_cooling_timescale, set_erasure_penalty, set_converge_multiplier, set_skip_thresh
 
 mutable struct DecodeParams
     lat_thresh :: Float64
