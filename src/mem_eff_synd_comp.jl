24408f5bbd8eaa826c387f2db74cc4df57fa20b9
Jonathan A. White <jawhite@caltech.edu>
2023-09-11 16:15:08 -0700

    clearing works, but must reallocate to pass tests

--- src/mem_eff_synd_comp.jl
+++ src/mem_eff_synd_comp.jl
@@ -15,32 +15,21 @@ function find_barcodes_mem_eff(pnts ::DataFrame,
 
 
     unprocessed_inrange_dots = fill(0, nv(g.g))
-    last_round_tree = make_KDTree(pnts[final_round_dots, :])
+    round_trees = [make_KDTree(pnts[get_cw_pos_inds(g, r), :]) for r in 1:g.n] 
+    last_round_tree = round_trees[end] #make_KDTree(pnts[final_round_dots, :])
 
     # Count how many dots in other rounds are in range of each dot in the last round
     for dot in final_round_dots
         for round in 1:(g.n-1)
-            unprocessed_inrange_dots[dot] += length(inrange(g.trees[round], [pnts.x[dot], pnts.y[dot]], g.lat_thresh))
+            unprocessed_inrange_dots[dot] += length(inrange(round_trees[round], [pnts.x[dot], pnts.y[dot]], g.lat_thresh))
         end 
     end
 
     
     # Count how many dots in the last round are within the search radius of each dot in previous round
-    #final_round_inrange_overlap_graph = SimpleWeightedGraph(nrow(pnts))
-    #final_round_inrange_computed_dots = copy(unprocessed_inrange_dots[final_round_dots])
     for dot in 1:(g.cw_pos_bnds[g.n]-1)
         inrange_last_round_dots = inrange(last_round_tree, [pnts.x[dot], pnts.y[dot]], g.lat_thresh, true)
         unprocessed_inrange_dots[dot] = length(inrange_last_round_dots)
-        """
-        for i in 1:length(inrange_last_round_dots)
-            for j in (i+1):length(inrange_last_round_dots)
-                doti = inrange_last_round_dots[i]
-                dotj = inrange_last_round_dots[j]
-                old_w = get_weight(final_round_inrange_overlap_graph, doti, dotj)
-                add_edge!(final_round_inrange_overlap_graph, doti, dotj, old_w + 1)
-            end
-        end
-        """
     end
     
 
@@ -52,22 +41,10 @@ function find_barcodes_mem_eff(pnts ::DataFrame,
     gene_nums = []
     # while there are dots in the last round that have not had their candidate barcodes searched for
     while length(unprocessed_last_round_dots) > 0
-    #for dot in final_round_dots
-        #println("dots to go: ", length(unprocessed_last_round_dots))
-        # find the final roud dot that han the fewest number (or any tied for fewest) of dots in the previous rounds that have 
-        # not yet contributed to a syndrome computation
-        """
-        dot_ind = argmin(final_round_inrange_computed_dots) # unprocessed final round indexing
-        dot = final_round_dots[dot_ind] # all points index
-        """
-
         # find the final round dot that has the fewest number of other uncomputed final round dots in
         # its range
         dot_ind = argmin(final_round_dot_n_uncomputed_neighbors[unprocessed_last_round_dots .- (g.cw_pos_bnds[g.n]-1)])
-        #println("dot_ind: ", dot_ind)
-        dot = unprocessed_last_round_dots[dot_ind]
-        #println("dot: ", dot)
-        
+        dot = unprocessed_last_round_dots[dot_ind]        
 
         # Compute syndromes for and find barcode candidates including that dot
         dot_barcode_candidates, dot_gene_nums = find_final_round_dot_barcode_candidates!(
@@ -80,9 +57,6 @@ function find_barcodes_mem_eff(pnts ::DataFrame,
             syndrome_block_bounds,
             dot
             )
-        #println("syndromes:")
-        #println(syndromes)
-        #println("dot_gene_nums: ", dot_gene_nums)
 
         append!(barcode_candidates, dot_barcode_candidates)
         append!(gene_nums, dot_gene_nums)
@@ -91,36 +65,21 @@ function find_barcodes_mem_eff(pnts ::DataFrame,
         # update final round neighbors graph
         final_round_dot_n_uncomputed_neighbors[inrange(last_round_tree, [pnts.x[dot], pnts.y[dot]], g.lat_thresh)] .-= 1
 
-        # update uncomputed inrange dots
-        """
-        for neighbor in neighbors(final_round_inrange_overlap_graph, dot) # all pnt indexing
-            overlap = get_weight(final_round_inrange_overlap_graph, dot, neighbor)
-            neighbor_ind = indexin(neighbor, final_round_inrange_computed_dots) # convert to unprocessed final round indexing
-            final_round_inrange_computed_dots[neighbor_ind] -= overlap
-            #final_round_inrange_computed_dots[neighbor - final_round_dots_start_m1] -= overlap
-        end
-        """
-
         # remove dot from list of unprocessed final round dots
-        #unprocessed_dot_index = indexin(dot, unprocessed_last_round_dots)
-        
-        deleteat!(unprocessed_last_round_dots, dot_ind)
-        #deleteat!(final_round_inrange_computed_dots, dot_ind)
-        
+        deleteat!(unprocessed_last_round_dots, dot_ind)        
 
-        #free space
-        #syndromes[dot] = []
-        """
+        #free space  
         for round in 1:(g.n-1)
-            for inrange_dot in inrange(g.trees[round], [pnts.x[dot], pnts.y[dot]], g.lat_thresh)
-                @inbounds unprocessed_inrange_dots[inrange_dot] -= 1
-                if unprocessed_inrange_dots[inrange_dot] == 0
-                    syndromes[inrange_dot] = []
-                    syndrome_block_bounds[inrange_dot] = []
+            for inrange_dot in inrange(round_trees[round], [pnts.x[dot], pnts.y[dot]], g.lat_thresh)
+                inrange_dot_ind = inrange_dot + g.cw_pos_bnds[round] - 1
+                unprocessed_inrange_dots[inrange_dot_ind] -= 1
+                if unprocessed_inrange_dots[inrange_dot_ind] == 0
+                    syndromes[inrange_dot_ind] = []
+                    syndrome_block_bounds[inrange_dot_ind] = []
                 end
             end
         end
-        """
+        
     end
     #println("barcode_candidates:")
     #println(barcode_candidates)
@@ -155,6 +114,7 @@ function find_final_round_dot_barcode_candidates!(
             barcode_candidate = recursive_get_synd_neighbors_mem_eff(pnts, g, dot, i, syndromes, len_syndromes)
             #println("barcode_candidate: ", barcode_candidate)
             if ismissing(barcode_candidate)
+                #println("missing barcode candidate")
                 continue
             end
             cw = pnts.coeff[barcode_candidate]
@@ -164,17 +124,6 @@ function find_final_round_dot_barcode_candidates!(
             end
         end
     end
-    
-    # decrease counters for number of unprocessed inrange final round dots for each previous round dot inrange 
-    # of the dot inputed to this function
-    """
-    for tree in g.trees
-        early_dots = inrange(tree, [pnts.x[dot], pnts.y[dot]], g.lat_thresh)
-        unprocessed_inrange_dots[early_dots] .-= 1
-    end
-    """
-
-    # clear allocated syndrome components for dots not in the last round that have had all final round dots in their range processed
 
     return [barcode_candidates, gene_nums]
 end
@@ -198,7 +147,7 @@ function recursive_syndrome_computation!(
     # elif not allocated
     else
         # if all in range final round dots processed
-        if unprocessed_inrange_dots[dot] == 0
+        if false #unprocessed_inrange_dots[dot] == 0
             return 
         #elseif dot in first round
         elseif dot < g.cw_pos_bnds[2]
@@ -212,7 +161,7 @@ function recursive_syndrome_computation!(
             dot_neighbors = neighbors(g, dot)
             for dot in dot_neighbors
                 recursive_syndrome_computation!(pnts, g, syndromes, len_syndromes, unprocessed_inrange_dots, syndrome_block_bounds, dot)
-                @inbounds nsyndc += length(syndromes[dot])
+                @inbounds nsyndc += len_syndromes[dot] #length(syndromes[dot])
             end
             # allocate syndome component array of length nsnd
             @inbounds syndromes[dot] = fill(pnts.sc[dot], nsyndc)
@@ -311,7 +260,7 @@ function get_synd_neighbors_mem_eff(
         #if the syndrome includes a component from this particular neighbor
         if synd_ind <= cum_n_syndromes
             # find the index of the neighbors syndrome component of interest
-            @inbounds neighbor_synd_ind = synd_ind - cum_n_syndromes + length(syndromes[neighbor])
+            @inbounds neighbor_synd_ind = synd_ind - cum_n_syndromes + len_syndromes[neighbor]
             return [neighbor, neighbor_synd_ind]
         end
     end
