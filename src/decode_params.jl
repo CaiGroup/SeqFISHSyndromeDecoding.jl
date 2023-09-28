0d1e655f120b70f6fe9e79046d74bb8f59b6d302
jonat <jawhite@caltech.edu>
2023-09-28 15:02:37 -0700

    add export

--- src/decode_params.jl
+++ src/decode_params.jl
@@ -1,5 +1,5 @@
 export set_xy_search_radius, set_z_search_radius, set_n_allowed_drops, set_lat_var_cost_coeff,
-set_z_var_cost_coeff, set_lw_var_cost_coeff, set_s_var_cost_coeff,
+set_z_var_cost_coeff, set_lw_var_cost_coeff, set_s_var_cost_coeff, set_zeros_probed,
 set_mip_sa_thresh, set_free_dot_cost, set_n_chains, set_l_chains, set_cooling_factor,
 set_cooling_timescale, set_erasure_penalty, set_converge_multiplier, set_skip_thresh, set_skip_density_thresh
 
